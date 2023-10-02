906821ccc78f5b8cba0b7913272f29be25b650e4
Umair <mrumairhassan@outlook.com>
2023-10-02 18:30:12 +0500

    types added

---
+++ step 2/javascript_types.js
@@ -0,0 +1,12 @@
+let str1 = 'Laurence'; 
+let str2 = "Svekis"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+
+console.log(typeof(str1));  //string
+console.log(typeof(str2));  //string
+console.log(typeof(val1));  //undefined
+console.log(typeof(val2));  //null
+console.log(typeof(myNum)); //number
