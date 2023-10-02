f74aad77a4fbdec5ef88506acebf204bc8aeb11d
Umair <mrumairhassan@outlook.com>
2023-10-02 18:35:13 +0500

    add 2.2

---
+++ step 2/javascript_types 2.2.js
@@ -0,0 +1,7 @@
+const myName = "Umair";
+const myAge = 27;
+const coder = true;
+const message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
+console.log(message); 
+
+// Hello my name is "Umair" I am "27" years old and I cancode JavaScript: "true".
