19da7cc412d669d0afdf8fd0e7b806f59908d628
Umair <mrumairhassan@outlook.com>
2023-10-02 17:22:41 +0500

    add step 2

--- step00_helloWorld.js
+++ step00_helloWorld.js
@@ -1,2 +1,2 @@
-let a = "Hello World".
-console.log(a);
+let a = "Hello World";
+console.log(a);   //'Hello World'
---
+++ step01_alert.js
@@ -0,0 +1,15 @@
+let a = prompt("Hello this is Prompt");
+//  = prompt
+let status = a;
+let scared = a;
+
+if (status === "new") {
+  console.log("Welcome to JavaScript!");}
+  if (scared === "now") {
+    console.log("Don't worry you will be fine!");}
+//   } else {
+//     console.log("You're brave! You are going to do great!");
+//   }
+// } else {
+//   console.log("Welcome back, I new you'd like it!");
+// }
