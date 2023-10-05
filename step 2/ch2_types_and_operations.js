7979e42d63eddb69859f827ef4f95c1d7741059c
Umair <mrumairhassan@outlook.com>
2023-10-05 16:09:20 +0500

    add ch2_types_and_operations.js

---
+++ step 2/ch2_types_and_operations.js
@@ -0,0 +1,11 @@
+let nr1 = 12;
+let nr2 = 14;
+let str1 = "Hello ";
+let str2 = "addition";
+let result1 = nr1 + nr2;
+let result2 = str1 + str2;
+console.log(result1, result2);  //26 'Hello addition'
+
+// let nr1 = 20;
+// let nr2 = 4;
+// let str1 = "Hi ";
