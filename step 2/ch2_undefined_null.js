550b35c20d6825e52c566f501e9e0efacc11daf2
Umair <mrumairhassan@outlook.com>
2023-10-05 17:04:34 +0500

    add ch2_variables_basics.js

---
+++ step 2/ch2_undefined_null.js
@@ -0,0 +1,11 @@
+let unassigned;
+console.log(unassigned);  //undefined
+
+let terribleThingToDo = undefined;
+let lastname;
+console.log("Same undefined:", lastname === terribleThingToDo); //'Same undefined:' true
+
+let betterOption = null;
+console.log("Same null:", lastname === betterOption);  //'Same null:' false
+
+let empty = null;
---
+++ step 2/ch2_variables_basics.js
@@ -0,0 +1,12 @@
+let firstname = "Maria";
+firstname = "Jacky";
+
+let nr1 = 12;
+var nr2 = 8;
+nr1 = 20; // This is fine
+nr2 = 10; // This is fine
+const pi = 3.14159;
+
+// throws a TypeError
+const someConstant = 3;
+someConstant = 4;      // canst cant be change
