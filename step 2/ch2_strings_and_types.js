a061e7cf99bda05fc8c02c6a56f59d1402d48885
Umair <mrumairhassan@outlook.com>
2023-10-05 16:05:35 +0500

    Add ch2_strings_and_types.js

---
+++ step 2/ch2_strings_and_types.js
@@ -0,0 +1,13 @@
+let str = "Hello, what's your name? Is it \"Mike\"?"; 
+console.log(str); //`Hello, what's your name? Is it "Mike"?`
+let str2 = 'Hello, what\'s your name? Is it "Mike"?';
+console.log(str2); //`Hello, what's your name? Is it "Mike"?`
+
+let str3 = "New \nline."
+let str4 = "I'm containing a backslash: \\!";
+console.log(str3);
+// 'New 
+// line.'
+console.log(str4); //"I'm containing a backslash: \!"
+
+let strs = "Hello";// Hello
