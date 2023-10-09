8ad8b90e2a3ee94c2bb7d3363926ba1f75d8c57f
Umair <mrumairhassan@outlook.com>
2023-10-09 19:55:32 +0500

    add array example

---
+++ step 3/ch3_arrays.js
@@ -0,0 +1,11 @@
+arr1 = new Array("purple", "green", "yellow");
+arr2 = ["black", "orange", "pink"];
+
+arr3 = new Array(10);
+arr4 = [10];
+
+console.log(arr3);
+console.log(arr4);
+
+cars = ["Toyota", "Renault", "Volkswagen"];
+console.log(cars[0]);
