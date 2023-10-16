aed039da030c1e682f2c4dff1cbcc48321623555
Umair <mrumairhassan@outlook.com>
2023-10-17 01:06:13 +0500

    Add object destructuring code

---
+++ step4/Es6_ObjectDestructuring.js
@@ -0,0 +1,18 @@
+const address = {
+  
+    city: '',
+    country: '',
+    street: ''
+  }
+  
+  const ctiy = address.city;
+  const country = address.city;
+  const street = address.city;
+  
+  // destructure this above code
+  
+//   const {city , country, street} = address;
+  
+  // if you want to change the const name use ayles
+  
+//   const {city:cd} = address;
