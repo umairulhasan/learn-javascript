65f660b43e0003362982dbaf3680a6c7f4f8d263
Umair <mrumairhassan@outlook.com>
2023-10-17 00:38:13 +0500

    Add object and this keyword example

---
+++ step4/ES6_Object.js
@@ -0,0 +1,41 @@
+// in javascript
+const Person = {
+    name : 'Umair',
+    walk: function() {}  // function in object but in oop we call it method because they declear inside the object
+};
+ 
+
+
+// in ES6 formation
+const Person2 = {
+
+    name : 'Umair',
+    walk() {}
+}
+
+// 2 ways for access the object properties 
+
+Person.name   // Number 1
+Person.walk() // Number 1
+
+
+Person['walk'] // Number 2
+Person['name'] // Number 2
+
+Person.name =  "Umairc" // for dynamic change   
+
+
+                                           // This  key word
+
+// const Person = {
+
+//     name : 'Umair',
+//     walk() {console.log(this)}
+// }
+// Person.walk();
+
+// output
+// { name: 'Umair', walk: ƒ walk() }
+
+// This always return a reference to the current object 
+// in this case they return the person object but they work only in method (Imp)not in function
